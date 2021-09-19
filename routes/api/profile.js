7dfea340df0c3dc6b46c34048da80d2648f0f50b
Kiran Anand <liteninkiran@hotmail.com>
2021-09-19 13:47:01 +0100

    Add experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -154,4 +154,59 @@ const deleteProfileByUserId = async (req, res) => {
 
 router.delete('/user/:user_id', auth, checkObjectId('user_id'), deleteProfileByUserId);
 
+// @route    PUT api/profile/experience
+// @desc     Add profile experience
+// @access   Private
+
+const experienceValidators = [
+    check('title', 'Title is required').notEmpty(),
+    check('company', 'Company is required').notEmpty(),
+    check('from', 'From is required').notEmpty(),
+    check('to', 'To date must be after from date').custom((value, { req }) => (req.body.from ? value > req.body.from : true)),
+];
+
+const addExperience = async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description,
+    } = req.body;
+
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+
+}
+
+router.put('/experience', [auth, experienceValidators], addExperience);
+
 module.exports = router;
