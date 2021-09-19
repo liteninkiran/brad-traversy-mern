117dbae1b0474114bb3268067b11c11e1b5d4363
Kiran Anand <liteninkiran@hotmail.com>
2021-09-19 17:42:29 +0100

    Add post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check } = require('express-validator');
 
+const { addPost } = require('../../controllers/PostController.js');
 
-// @route   GET api/posts
-// @desc    Test route
-// @access  public
-router.get('/', (req, res) => res.send('Post route'));
+const addPostValidators = [
+    check('text', 'Text is required').notEmpty(),
+];
+
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+router.post('/', [auth, addPostValidators], addPost);
 
 module.exports = router;
