fd66897f2deb9a41b93cf8945d6fbd254bc1f1b0
Kiran Anand <liteninkiran@hotmail.com>
2021-09-20 17:51:45 +0100

    Clear profile on logout

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     LOGOUT,
+    CLEAR_PROFILE,
 } from './types';
 
 // Load User
@@ -76,4 +77,7 @@ export const login = (email, password) => async (dispatch) => {
 };
 
 // Logout
-export const logout = () => ({ type: LOGOUT });
+export const logout = () => (dispatch) => {
+    dispatch({ type: CLEAR_PROFILE });
+    dispatch({ type: LOGOUT });
+}
